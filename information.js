a03fc0830f602a3e457a2262328b12813aec8c84
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-10-03 15:19:59 +0200

    introduce myself through cowsay

---
+++ information.js
@@ -0,0 +1,7 @@
+const profil = {
+    name: "Alexis",
+    campus: "Lille",
+    school: "Wild Code School",
+}
+
+module.exports = profil;
---
+++ script.js
@@ -0,0 +1,9 @@
+const myInfo = require('./information.js');
+
+let cowsay = require('cowsay');
+
+console.log(cowsay.say({
+    text : (`Hello my name is ${myInfo.name} and I am in a process of learnig at the ${myInfo.school} in ${myInfo.campus}.`),
+    e : "oO",
+    T : "U",
+}));
