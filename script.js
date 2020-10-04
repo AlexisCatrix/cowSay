d83ac52fb3943ccbb6a49287507993e9395f60d9
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-10-04 13:48:50 +0200

    fixed typo problem

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const myInfo = require('./information.js');
 let cowsay = require('cowsay');
 
 console.log(cowsay.say({
-    text : (`Hello my name is ${myInfo.name} and I am in a process of learnig at the ${myInfo.school} in ${myInfo.campus}.`),
+    text : (`Hello my name is ${myInfo.name} and I am in a process of learning at the ${myInfo.school} in ${myInfo.campus}.`),
     e : "oO",
     T : "U",
 }));
